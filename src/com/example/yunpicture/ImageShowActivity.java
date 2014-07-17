b3e1c39087ca42db7c9274155e04b565de75947d
mafagan <mahuaguan@126.com>
2014-07-17 16:30:03 +0800

    imageview cast to listview

--- src/com/example/yunpicture/ImageShowActivity.java
+++ src/com/example/yunpicture/ImageShowActivity.java
@@ -3,19 +3,55 @@ package com.example.yunpicture;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class ImageShowActivity extends Activity {
+	private static String[] textData = new String[]{
+		"text #1",
+		"text #2",
+		"text #3"
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pic1);
 		
 		ListView filterListView = (ListView) findViewById(R.id.filterlv);
-		ListItemAdapter filterAdapter = new ListItemAdapter(this);
-		filterListView.setAdapter(filterAdapter);
+		//ListItemAdapter filterAdapter = new ListItemAdapter(this);
+		//filterListView.setAdapter(filterAdapter);
 		
 	}
+	
+	private BaseAdapter mAdapter = new BaseAdapter() {
+		
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return textData.length;
+		}
+	};
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
